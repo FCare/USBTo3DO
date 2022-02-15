51cda894153452241b8ad52bac62fcec866baf98
Francois CARON <francois.caron@renault.com>
2022-02-15 12:02:02 +0100

    Enable USB_HOst with Dualshock4 support

--- src/USB_Host.c
+++ src/USB_Host.c
@@ -1,5 +1,16 @@
 #include "USB_Host.h"
 
+#include "bsp/board.h"
+
+enum  {
+  BLINK_NOT_MOUNTED = 250,
+  BLINK_MOUNTED = 1000,
+  BLINK_SUSPENDED = 2500,
+};
+
+static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
+
+static void led_blinking_task(void);
 
 void USB_Host_init() {
     board_init();
@@ -22,4 +33,17 @@ void USB_Host_loop()
 #if CFG_TUH_HID
   hid_app_task();
 #endif
+}
+
+static void led_blinking_task(void)
+{
+  static uint32_t start_ms = 0;
+  static bool led_state = false;
+
+  // Blink every interval ms
+  if ( board_millis() - start_ms < blink_interval_ms) return; // not enough time
+  start_ms += blink_interval_ms;
+
+  board_led_write(led_state);
+  led_state = 1 - led_state; // toggle
 }
