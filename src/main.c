0cf8ab83e4b5c03b221a1e6f091b67dc4e77928c
Francois CARON <francois.caron@renault.com>
2022-02-15 09:19:01 +0100

    Add initial files

---
+++ src/main.c
@@ -0,0 +1,19 @@
+﻿#include "LCD_Test.h"   //Examples
+
+int main(void)
+{
+    //OLED
+    // OLED_1in3_C_test();
+    //OLED_2in23_test();
+
+
+    //LCD
+	LCD_0in96_test();
+    //LCD_1in14_test();
+    //LCD_1in14_V2_test();
+	//LCD_1in3_test();
+    //LCD_1in44_test();
+    //LCD_1in8_test();
+    //LCD_2in_test();
+    return 0;
+}
